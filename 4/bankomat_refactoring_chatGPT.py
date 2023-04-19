4c4cd2fdea248facc6a6b496ca5fac52855ba56c
teachertyp <yurchikte@gmail.com>
2023-04-19 21:25:26 +0300

    погрався

--- 4/bankomat_refactoring_chatGPT.py
+++ 4/bankomat_refactoring_chatGPT.py
@@ -41,12 +41,12 @@ def bankomat(cash, nom):
     
     index = nom.index(last_nom)
     if temp_cash < 0:
-        #print("--------",temp_cash)
+
         if temp_cash*-1 in nom:
             nom_sel[nom.index(temp_cash*-1)] -= 1
-            #print(nom_sel)
+
             temp_cash = 0
-            #print("Видано",all_sum(nom_sel,nom))
+
             return
         else: #якщо раптом коли додали нову купюру залишок від'ємний, але більший ніж номінали
             nom_sel, temp_cash = minus(index, temp_cash, nom, nom_sel)
@@ -55,20 +55,15 @@ def bankomat(cash, nom):
 
 
     m = temp_cash // last_nom
-    #print(nom_sel)
-    #print ("Поточна купюра:",last_nom,", Залишок:",temp_cash," Беремо: ",nom[index],"-к", m)
     
     if temp_cash ==0:
         return
     if m == 0: #це означає, що грошей менше ніж номінал наступної купюри, але треба брати його, а від реши віднімати
-        #print("---",index,temp_cash)
         if nom_sel[index]<=10:
             nom_sel, last_nom = next_nom(nom_sel, index, last_nom, nom)
         bankomat(cash, nom)
     elif m < 10:
-         #print("m < 10")
          nom_sel[index] +=m
-         #print ("Залишилося:", temp_cash)
          if temp_cash > 0 and temp_cash % last_nom == 0:
              return
          else:
@@ -76,7 +71,6 @@ def bankomat(cash, nom):
          bankomat(cash, nom)       
     elif m > 10 and nom[index]<500: #якщо кількість купюр яку можна видати більше ніж 10
          nom_sel[index] = 10
-         #print (last_nom,":",temp_cash)
          if temp_cash > 0:
             index +=1
             last_nom = nom[index]
@@ -87,7 +81,6 @@ def bankomat(cash, nom):
         last_nom = nom[index]
         bankomat(cash, nom)
     else:#це означає, що я можу використать усі купюри даного номіналу
-        #print ("----")
         last_nom = nom[index]
         nom_sel[index] = m
         index +=1 
@@ -99,17 +92,17 @@ nom = [10,20,50,100,200,500,1000]
 
 nom_sel = [0,0,0,0,0,0,0]
 limit = 10
+cash = int(input("Введіть суму, яку хочете отримати: "))
+print("дррр-р-р-р-р-р-р-р-р-р-")
 temp_sum = 0 #скільки видали
 temp_cash = cash #це гроші, що залишилися
 index = 0
 last_nom= nom[0] #остання вибрана купюра
+
 if cash % 10 == 0 and cash > 0:
     while temp_cash>0 :
-        
         bankomat(cash, nom)
-        #print(nom_sel)
         if temp_cash < 0 or temp_cash == cash:
-            #print(nom_sel)
             break
         
 else:
@@ -124,5 +117,7 @@ for i in range(6):
         if i<6 and r>1 and nom[i+1]==2*nom[i] and nom_sel[i+1]>0:
             nom_sel[i] += 2
             nom_sel[i+1] -=1
+
 print(nom_sel)
 print("Запит:",cash,"Видано",all_sum(nom_sel,nom))
+print("Не забудьте забрати картку")
